cf2d89f93938523a6ab3fc5ff894ef7dbd9f06f6
shirakawa <49916591+attoiumani@users.noreply.github.com>
2021-09-08 20:06:48 +0900

    　

--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>{{ headerItems }}</div>
+  <div>
+    <p>all：{{ headerItems }}</p>
+    <p>ID：{{ id }}</p>
+  </div>
 </template>
 
 <script>
@@ -8,36 +11,38 @@ export default {
   data() {
     return {
       // APIを叩いて返ってきたコンテンツを格納する用に空の配列を用意
-      headerItems: [],
-    }
+      headerItems: Array,
+      id: Array,
+    };
   },
   mounted() {
     // mount時にヘッダ一覧を取得する処理を実行
-    this.getHeaders()
+    this.getHeaders();
   },
   methods: {
     getHeaders() {
       // Git管理しないAPIキーは、.envファイル内にて別途定義する
       // オプションとして、コンテンツの最大取得件数は100件と設定
       axios
-        .get('https://bbb.microcms.io/api/v1/blog', {
-          headers: { 'X-API-KEY': 'b946077c-5861-4db6-bb26-9cf8d183dedf' }
+        .get("https://bbb.microcms.io/api/v1/blog", {
+          headers: { "X-API-KEY": "b946077c-5861-4db6-bb26-9cf8d183dedf" },
         })
         .then((res) => {
           // 取得したコンテンツをコンポーネントのdata内に格納
-          this.headerItems = res.data.contents
+          this.headerItems = res.data.contents;
+          this.id = res.data.contents[0].id;
           // 取得したアイテムをシャッフル
           for (let i = this.headerItems.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1))
-            const tmp = this.headerItems[i]
-            this.headerItems[i] = this.headerItems[j]
-            this.headerItems[j] = tmp
+            const j = Math.floor(Math.random() * (i + 1));
+            const tmp = this.headerItems[i];
+            this.headerItems[i] = this.headerItems[j];
+            this.headerItems[j] = tmp;
           }
         })
         .catch((err) => {
-          console.log(err)
-        })
-    }
-  }
-}
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
